c8c990691deea783d143b29bad7c68e43a616963
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-11-08 01:49:41 +0000

    first commit

---
+++ app/phone-list/phone-list.component.js
@@ -0,0 +1,23 @@
+// Register the `phoneList` component on the `phoneList` module
+angular.module('phoneList')
+
+.component('phoneList', {
+  templateUrl: 'phone-list/phone-list.template.html',
+  controller: function PhoneListController(){
+    this.name = 'Elizabeth';
+    this.phones = [
+      {
+        name: 'Nexus S',
+        snippet: 'Fast just got faster with Nexus S.'
+      }, {
+        name: 'Motorola XOOM™ with Wi-Fi',
+        snippet: 'The Next, Next Generation tablet.'
+      }, {
+        name: 'MOTOROLA XOOM™',
+        snippet: 'The Next, Next Generation tablet.'
+      }
+    ];
+  }
+})
+
+// by default, components use $ctrl as the controller alias
